b205c5c8b3bf16ba3c55078c8ecb86aa19437026
Asif Jamal <asifjamal582005@gmail.com>
2025-12-10 20:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let prices = document.querySelectorAll("td.price");
+
 const getSum = () => {
-//Add your code here
-  
+    let sum = 0;
+
+    prices.forEach(function(p){
+        sum = sum + Number(p.innerText);
+    });
+
+    console.log(sum);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
